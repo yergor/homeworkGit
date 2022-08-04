456fdf5d7f778cc4cb5472d5b4c98a2a0d95fbb7
g.ericyan <yergor@mail.ru>
2022-08-05 00:06:58 +0500

    added FirstTest

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    void firstTest(){
+        System.out.println("Hello World!");
+    }
+
+    @Test
+    void conflict(){
+        Assertions.assertTrue(true);
+    }
+}
